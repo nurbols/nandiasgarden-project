8f28d8d60c2168cc58b7f39ced32726602fcd8ec
Nurbol Satibaldiev <satibaldievn25@gmail.com>
2023-09-06 15:49:17 +0500

    pizza deliver

---
+++ pizza/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path 
+#from .views import HomePageView, OrderPageView
+
+'''urlpatterns = [
+    path('', HomePageView.as_view(), name='home'), 
+    path('order/', OrderPageView.as_view(), name='order'), 
+]  ''' 
